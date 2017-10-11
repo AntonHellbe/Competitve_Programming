50c7bdbc8c605f7b9f62c30417adb04c191e960f
AntonHellbe <antonhellbe2@gmail.com>
2017-10-11 18:09:35 +0200

    fortsatt på TSP, rekursiv lösning med memoization(?) table

--- Currently solving/TSP.py
+++ Currently solving/TSP.py
@@ -1,62 +1,70 @@
 import sys
-
-
+import time
 directionDict = {'UPRIGHT': (1, 1), 'DOWNRIGHT':(-1, 1), 'RIGHT':(0, 1)}
-pathDict = {}
-memoryDict = {}
-
-def min_path_finder(start, row, col, matrix):
-    totalWeight = 0
-    row1 = start[0]
-    col1 = start[1]
-    totalWeight += matrix[row1][col1]
-    allPath = [(row1, col1)]
-    while(True):
-        currentPosition = allPath[len(allPath) - 1]
-        print(allPath)
-        savedRow1 = currentPosition[0]
-        savedCol1 = currentPosition[1]
-        best_path = []
-        for direct in directionDict.values():
-            row1 = savedRow1 + direct[0]
-            col1 = savedCol1 + direct[1]
-            if(row1 == - 1):
-                row1 = row - 1
-            elif(row1 == row):
-                row1 == 0
-            best_path.append(((row1, col1), matrix[row1][col1]))
-
-        temp = min(best_path, key= lambda item: item[1])
-        totalWeight += temp[1]
-        allPath.append(temp[0])
-        if(temp[0][1] == col - 1):
-            return allPath, totalWeight
 
 
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print ('%s function took %0.3f ms' % (f.__name__, (time2-time1)*1000.0))
+        return ret
+    return wrap
 
-    return allPath
 
-def min_path_finder2(node, row, col):
+def minimum_weight(best_path, col):
+    mymin = min(best_path, key = lambda item: item[col])
+    return [path for path in best_path if path[col] == mymin[col]]
+
+def go_right_down(y, x, row):
+    if(y + 1 == row):
+        return (0, x + 1)
+    return (y + 1, x + 1)
+
+def go_right_up(y, x, row):
+    if(y - 1 == -1):
+        return(row - 1, x + 1)
+    return (y - 1 , x + 1)
+
+def go_right(y, x, row):
+    return (y, x + 1)
+
+
+def min_path_finder(node, row, col):
 
     row1 = node[0]
     col1 = node[1]
-    nodeWeight = matrix[row1][col1
-                              ]
+
+
+    if(savedPositions[row1][col1] != 9999):
+        return (savedPositions[row1][col1], (row1, col1))
+
     if col1 == col - 1:
-        return 1
+        if len(paths[row1][col1]) == 0:
+            paths[row1][col1].append((row1, col1))
+        return (matrix[row1][col1], (row1, col1))
 
-    for direct in directionDict.values():
-        newRow = row1 + direct[0]
-        newCol = col1 + direct[1]
-        if(newRow == - 1):
-            newRow = row - 1
-        elif(newRow == row):
-            newRow = 0
 
-        if memoryDict[(newRow, newCol)] > memoryDict[(row1, col1)] + matrix[newRow][newCol]:
-            memoryDict[(newRow, newCol)] = memoryDict[(row1, col1)] + matrix[newRow][newCol]
-            min_path_finder2((newRow, newCol), row, col)
+    rightup_position = go_right_up(row1, col1, row)
+    rightdown_position = go_right_down(row1, col1, row)
+    right_position = go_right(row1, col1, row)
 
+    upright = min_path_finder(rightup_position, row, col)
+    downright = min_path_finder(rightdown_position, row, col)
+    right = min_path_finder(right_position, row, col)
+
+    bestvalue = min(upright, downright, right, key = lambda item: (item[0], item[1][0]))
+
+    savedPath = []
+    savedPath.extend(paths[bestvalue[1][0]][bestvalue[1][1]])
+    savedPath.append((row1, col1))
+
+    paths[row1][col1] = savedPath
+
+    savedPositions[row1][col1] = matrix[row1][col1] + bestvalue[0]
+
+    return (savedPositions[row1][col1], (row1, col1))
 
 
 
@@ -66,25 +74,58 @@ lines = [line.rstrip("\n") for line in sys.stdin]
 
 
 currentLine = 0
-data = list(map(int, lines[currentLine].split(" ")))
-row = data[0]
-col = data[1]
-matrix = []
-currentLine += 1
 
-for i in range(row):
-    temp = list(map(int, lines[currentLine].split(" ")))
-    matrix.append(temp)
+
+while(currentLine < len(lines)):
+    matrix = []
+    data = list(map(int, lines[currentLine].split(" ")))
+    best_paths = []
+    row = data[0]
+    col = data[1]
+
+    paths = [[list() for j in range(col)] for i in range(row)]
+
+    savedPositions = [[9999 for i in range(col)] for j in range(row)]
+
     currentLine += 1
 
-for i in range(row):
-    for j in range(col):
-        visitedDict[(i, j)] = False
-        memoryDict[(i, j)] = 9999
+    for i in range(row):
+        temp = list(map(int, lines[currentLine].split(" ")))
+        matrix.append(temp)
+        currentLine += 1
+
+    for i in range(row):
+        min_path_finder((i,0), row, col)
+
+    for i in range(len(paths)):
+        # print(paths[i][0])
+        best_paths.append(paths[i][0])
+        # print(paths[i][0])
+
+    for row in best_paths:
+        print(row)
+
+    # for i in range(row):
+    #     memoryDict[(i,0)] = matrix[i][0]
+    #     min_path_finder2((i,0), row, col)
+
+    # temp = minimum_weight(best_path, col)
 
 
 
-memoryDict[(0,0)] = matrix[0][0]
-min_path_finder2((0,0), row, col)
-for k, v in memoryDict.items():
-    print(k, v)
+    # for path in temp:
+        # print(path)
+
+
+
+    # temp = min(best_path, key = lambda item: item[col])
+    #
+    # for j in range(len(temp) - 1):
+    #     if(j == len(temp) - 2):
+    #         print(temp[j][0] + 1)
+    #     else:
+    #         print(temp[j][0] + 1, end=" ")
+    #
+    # print(temp[len(temp) - 1])
+
+    # currentLine += 1
